d13ecf31fc3d8fc6216c09f40f423551b2e70674
Carolyn Vasisko <carolyn@mothermedia.com>
2018-05-12 13:21:56 -0500

    Initial server file coding

--- server.js
+++ server.js
@@ -1,13 +1,20 @@
-
 var express = require("express");
+var path = require('path');
+var db = require("./models");
 var bodyParser = require("body-parser");
 var methodOverride = require("method-override");
 var exphbs = require('express-handlebars');
-
-
+var expressValidator = require ('express-validator');
+var flash = require('connect-flash');
+var session = require('express-session');
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
 
 var app = express();
+
 var PORT = process.env.PORT || 3000;
+
+
 
 // Serve static content for the app from the "public" directory in the application directory.
 app.use(express.static("public"));
@@ -30,7 +37,9 @@ var routes = require("./controllers/winController.js");
 app.use("/", routes);
 
 // Start our server so that it can begin listening to client requests.
+db.sequelize.sync({force: true}).then(function(){
 app.listen(PORT, function() {
-  // Log (server-side) when our server has started
-  console.log("Server listening on: http://localhost:" + PORT);
+ // Log (server-side) when our server has started
+ console.log("Server listening on: http://localhost:" + PORT);
+});
 });
