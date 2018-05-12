f1aa33c2e2306412ac1a84fa8ec5a50051c1b8e8
Sbedner <33232297+Sbedner@users.noreply.github.com>
2018-05-12 11:59:54 -0500

    updating packages

--- server.js
+++ server.js
@@ -4,9 +4,10 @@ var bodyParser = require("body-parser");
 var methodOverride = require("method-override");
 var exphbs = require('express-handlebars');
 
-var PORT = process.env.PORT || 3000;
+
 
 var app = express();
+var PORT = process.env.PORT || 3000;
 
 // Serve static content for the app from the "public" directory in the application directory.
 app.use(express.static("public"));
