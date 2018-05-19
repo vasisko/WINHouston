57e5bb028d667552730a19e2e95f257868f12456
Sbedner <33232297+Sbedner@users.noreply.github.com>
2018-05-19 14:00:35 -0500

    updating resource.js and databas ready to deploy

--- public/js/winList.js
+++ public/js/winList.js
@@ -1,34 +1,51 @@
 
-$(document).ready(function() {
-    $("#resourceTable").hide();
-    
-    //Event listener to list resources
-      $(document).on("click", "#searchAll", searchAll);
-      $(document).on("click", "#reset", reset);
-    
+$(document).ready(function () {
+  $("#resourceTable").hide();
 
-    function searchAll(event) {
-      event.preventDefault();
-      
-      
-      getResources(); 
-    }
+  //Event listener to list resources
+  $(document).on("click", "#searchAll", searchAll);
+  $(document).on("click", "#reset", reset);
 
-    function reset(event) {
-      location.reload();
-    }
-    function getResources() {
-  
 
-        $.get("/getresources", function(data) {
-            console.log(data);
-            $("#resourceForm").hide();
-            $("#resourceTable").show();
-            $("#resourceTable").text(data[0].address);
-        });
-      }
+  function searchAll(event) {
+    event.preventDefault();
+    getResources();
 
-    
+  }
+
+  function reset(event) {
+    location.reload();
+  }
+  function getResources() {
+
+
+    $.get("/getresources", function (data) {
+      console.log("data is", data);
+
+      if ($('#checkNW').is(":checked")) {
+        for (i = 0; i < data.length; i++) {
+          if (data[i].location === $("#checkNW").val()) {
+            console.log("NW works!");
+           }
+          console.log("it is checked");
+          }
+
+        // $("#resourceForm").hide();
+        // $("#resourceTable").show();
+        // // $("#address").text(data[0].address);
+        // var brandon = $("#checkNW").val().trim();
+        // console.log("brandon is:", brandon);
+        // for (i = 0; i < data.length; i++) {
+        //   if (data[i].location = $("#checkNW").val()) {
+        //     console.log("NW works!");
+        //       }
+
+            }
+
+      });
+  }
+
+
 
 
 
